994296c81e5b43a882413b6eb2278e5cb0a62555
To1Ms <151936276+To1Ms@users.noreply.github.com>
2024-02-12 16:35:43 +0100

    Update tools.py

--- database/tools.py
+++ database/tools.py
@@ -1,60 +1,62 @@
 import os
 import sqlite3
 from sqlite3 import *
+from pathlib import Path
 
+def check_db_exists(db_name) -> bool:
+    try:
+        conn = sqlite3.connect(f"{db_name}.db")
+        conn.close()
+        return True
+    except FileNotFoundError:
+        print("DB doesn't exist")
+        return False
 
-"""
-struttura:
-
-def create or open():
-    if connectionexist:
-        opendb
+def create_or_open_db(museo):
+    conn = None
+    museo_path = Path(__file__).resolve().parent / f"{museo}.db"
+    if check_db_exists:
+        conn = connection_exists(museo_path)
     else:
-        createdb
-
-opendb:
-    connection = sql...
-    return connection
-
-createdb:
-    connection = sql
-    db = sql.create..
-    return connection
-
-connectionexists(db_name):
-    interroga il server sql se esiste db_name
-    yes:
-        return true
-    no:
-        return false
-
-"""
-
-
-def _create_database(museo):
+        conn = create_database(museo)
+def create_database(museo):
     try:
         conn = sqlite3.connect(museo)
         conn.close()
         print('db creato')
-    except sqlite3.OperationalError as e:
+    except sqlite3.Error as e:
         if 'no such table' in str(e):
             return False
         raise
     else:
         return True
 
-def connection_exists(museo):
-    database = os.path.join(os.path.dirname(__file__), museo)
-    return _create_database(database)
+def connection_exists(museo) -> sqlite3.Connection:
+    conn = None
+    try:
+        if os.path.isfile(f"{museo}.db"):
+            conn = sqlite3.connect(f"{museo}.db", check_same_thread=False)
+            cursor = conn.cursor()
+    except FileNotFoundError as err:
+        print(f"Errore: Database '{museo}' non trovato.")
+        print(err)
+    except Exception as err:
+        print(f"Errore imprevisto durante l'apertura del database '{museo}'. Dettagli: {err}")
+    finally:
+        if conn is not None:
+            conn.close()
+    return conn
 
-def create_or_open_db(museo):
-    if connection_exists(museo):
-        return
-    conn = sqlite3.connect(museo)
+# def create_or_open_db(museo):
+#     conn = None
+#     museo_path = Path(__file__).resolve().parent / f"{museo}.db"
+#     if connection_exists(museo):
+#         conn = connect()
+#     conn = sqlite3.connect(museo)
 
 
 def create_tables():
-    conn = connect(os.path.join(os.path.expanduser("~"), "Desktop", "opere.db"))
+    conn = connect(os.path.join(os.path.expanduser("~"), "Desktop", "museo.db"))
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -84,7 +86,9 @@ def load_data(opere):
     conn.commit()
     conn.close()
 
+
 if __name__ == "__main__":
     create_tables()
     opere = []
     load_data(opere)
+
