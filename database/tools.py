b33c7d9dd63b200737dbfb29a59927c11b658171
To1Ms <151936276+To1Ms@users.noreply.github.com>
2024-02-13 10:56:51 +0100

    Update tools.py

--- database/tools.py
+++ database/tools.py
@@ -1,13 +1,14 @@
 import contextlib
 import os
 import sqlite3
+from pathlib import Path
 
-def check_db_exists(museo, opere):
-    filename = os.path.join(museo, f"{opere}.db")
+def check_db_exists(museo, mostra):
+    filename = os.path.join(museo, f"{mostra}.db")
     if not os.path.exists(filename):
         return False
     try:
-        conn = sqlite3.connect(filename)
+        conn = sqlite3.connect(mostra)
         conn.close()
         return True
     except sqlite3.OperationalError as e:
@@ -16,31 +17,31 @@ def check_db_exists(museo, opere):
         else:
             raise
 
-def create_or_open_db(opere, db_folder="Museo"):
-    filename = os.path.join(db_folder, opere)
+def create_or_open_db(mostra, db_folder="Museo"):
+    filename = os.path.join(db_folder, mostra)
     if not os.path.exists(filename):
         try:
             conn = sqlite3.connect(f"{filename}")
-            print(f"Created new database {opere}.")
+            print(f"Created new database {mostra}.")
         except Exception as e:
-            print(f"Unable to create database '{opere}' due to:", str(e))
+            print(f"Unable to create database '{mostra}' due to:", str(e))
             return False
     else:
         try:
             conn = sqlite3.connect(
                 filename)  # controllare perché mi dice che è un Unexpected argument per il check_last_error
-            print(f"Opened existing database {opere}.")
+            print(f"Opened existing database {mostra}.")
         except Exception as e:
-            print(f"Unable to open database '{opere}', reason:", str(e))
+            print(f"Unable to open database '{mostra}', reason:", str(e))
             return False
-    contextlib.closing(opere)
+    contextlib.closing(mostra)
     return conn
 
 
-def connection_exists(opere):
+def connection_exists(mostra):
     conn = None
     try:
-        conn = create_or_open_db(opere)
+        conn = create_or_open_db(mostra)
         cursor = conn.cursor()
         cursor.execute("PRAGMA table info(opere);").fetchone()
         conn.close()
@@ -55,18 +56,18 @@ def connection_exists(opere):
             conn.close()
 
 
-def create_db_conn(opere, db_folder="museo"):
-    if connection_exists(opere):
-        conn = opendb(opere)
+def create_db_conn(mostra, db_folder="museo"):
+    if connection_exists(mostra):
+        conn = opendb(mostra)
     else:
-        conn = createdb(opere, db_folder)
+        conn = createdb(mostra, db_folder)
 
     return conn
 
 
-def opendb(opere):
+def opendb(mostra):
     try:
-        conn = create_or_open_db(opere)
+        conn = create_or_open_db(mostra)
     except Exception as e:
         print(f"Can't connect to db '{e.__str__()}'. Check if file exist.")
         return None
@@ -74,8 +75,8 @@ def opendb(opere):
         return conn
 
 
-def createdb(opere, db_folder="museo"):
-    conn = create_or_open_db(opere, db_folder)
+def createdb(mostra, db_folder="museo"):
+    conn = create_or_open_db(mostra, db_folder)
     if conn is not None:
         cursor = conn.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS Opere(
@@ -89,3 +90,36 @@ def createdb(opere, db_folder="museo"):
         conn.commit()
         conn.close()
     return conn
+
+
+FIle_DB = Path(__file__).parent / "mostra"
+
+if __name__ == "__main__":
+    opere = "evento"
+    # mi da problema Shadows name 'mostra' from outer scope
+
+    if not check_db_exists(opere, FIle_DB):
+        print(f"Creating new DB for {opere}")
+        conn = create_db_conn(opere, str(FIle_DB))
+        if conn is not None:
+            print("Database created or open correctly.")
+
+        else:
+            print("Impossible to find a connection")
+
+    else:
+        print(f"The DB for {opere} already exist")
+
+# Qui sotto metto gli errori comparsi fino ad ora
+# nel caso vogliate controllare
+# line 45 AttributeError: 'bool' object has no attribute 'cursor'
+# at     cursor = conn.cursor()
+#              ^^^^^^^^^^^
+# line 103 same error at     conn = create_db_conn(opere, str(FIle_DB))
+#                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# line 60, in create_db_conn
+#     if connection_exists(mostra):
+#        ^^^^^^^^^^^^^^^^^^^^^^^^^
+# line 56, in connection_exists
+#     conn.close()
+#     ^^^^^^^^^^
