cbd7f7ad17211fdb3fe4142ed083f03ef547dfa5
Netherfield <barberisjacopo@gmail.com>
2024-02-13 11:00:07 +0100

    pre flask integration

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,73 @@
+# Copyright (c) 2023 Jules aka Netherfield
 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import os
+import argparse
 
 
 from database.yikiparse import wikiscrape
 
+def clean_env():
+    try:
+        # clean up environment at the end of execution
+        print("Removing temporary files")
+        os.system("pyclean .")
+        print("Removed temporary files")
+    except:
+        print("pyclean . \nFailed execution. Exiting...")
+
 # start app
 def main():
-    print()
-# check arguments
+    """
+    Parses arguments
+        bapada [-h] [--scrape] [--no-db] [--test-db] [--update] [--link LINK] ...
+    Currently only --scrape and -h are implemented
+    """
+    # bapada [-h] [--scrape] [--no-db] [--test-db] [--update] [--link] ...
+    # for now just --scrape
+    parser = argparse.ArgumentParser(
+                        prog="BaPaDa",
+                        description="Museum Webapp",
+                        epilog="Thank you fro using BaPaDa")
+    # store_true means that if --scrape then scrape=True, if not included then scrape=False
+    parser.add_argument("--scrape", "-s", dest="scrape", action="store_true",
+                        help="scrape data from [--link] (this can take a while)")
+    args = parser.parse_args()
 
-# check db
+    if args.scrape:
+        print("""You are trying to scrape the database again.
+              It would take a lot of time so please reconsider.
+              The flag is fake anyway.""")
+    
+    # if args.db:
+    #   ...create db or don't idk
+    
 
-# scrape --if
 
-# createdb --if
 
+    clean_env()
+    
 # launch flask
-
 # wait
-
 # launch chrome/ff/edge
+if __name__ == "__main__":
+    main()
+
 
 
